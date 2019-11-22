e9b923e883393870de69f0bccf8941a2d3b5d7bb
Mr.Zhao <39028357+zzq275913866@users.noreply.github.com>
2019-11-22 23:01:49 +0800

    Add files via upload

--- course4/MM1.py
+++ course4/MM1.py
@@ -1,121 +1,101 @@
-import abc
 import random
-
-
-class Event:
-    time = 0
-
-    def lt(self, obj2):
-        return (self.time <= obj2.time)
-
-    @abc.abstractmethod
-    def execute(self, sim):
-        pass
+import bisect
 
 
 class EventList:
     elements = []
 
     def ins(self, x):
-        i = 1
-        length = len(self.elements)
-        while (i <= length and (self.elements[i-1].time < x.time)):
-            i = i + 1
+        bisect.insort(self.elements, x)
 
+    def remove_first(self):
         if len(self.elements) == 0:
-            self.elements.append(x)
-        else:
-            self.elements.insert(i-1, x)
-
-    def removefirst(self):
-
-        if len(self.elements) == 0:
-            ev = []
             return
-
-        ev = self.elements[0]
-        del self.elements[0]
+        ev = self.elements.pop(0)
         return ev
 
 
-class GenePoisEv(Event):
-    q = []
+class Simulator:
+    time = 0
+    sim_limit = 0
+    event_list = ()
 
-    def execute(self, sim):
-        packet = Packet(self.time)
-        self.q.insertQ(packet, sim)
+    def now(self):
+        return self.time
 
-        interarrivalTime = random.expovariate(0.2)
-        self.time = self.time + interarrivalTime
-        sim.insertEv(self)
+    def insert_ev(self, ev):
+        self.event_list.ins(ev)
+
+    def do_all_events(self):
+        ev = self.event_list.remove_first()
+        while ev is not None:
+            self.time = ev.time
+            if self.time > self.sim_limit:
+                break
+            ev.execute(self)
+            ev = self.event_list.remove_first()
 
 
 class Packet:
     created = ''
     sent = ''
-    delivd = ''
 
     def __init__(self, created):
         self.created = created
 
 
+class GenePoisEv:
+    time = 0
+    q = ()
+
+    def __lt__(self, obj2):
+        return self.time <= obj2.time
+
+    def execute(self, sim):
+        packet = Packet(self.time)
+        self.q.insert_q(packet, sim)
+
+        inter_arrival_time = random.expovariate(0.2)
+        self.time = self.time + inter_arrival_time
+        sim.insert_ev(self)
+
+
 class Que:
     que = []
-    s = []
+    s = ()
 
-    def insertQ(self, packet, sim):
+    def insert_q(self, packet, sim):
         if self.s.packetBeingServed is None:
-            self.s.insertServ(packet, sim)
+            self.s.insert_server(packet, sim)
         else:
             self.que.append(packet)
 
     def remove(self):
-        pac = self.que[0]
-        del self.que[0]
+        pac = self.que.pop(0)
         return pac
 
 
-class ServExpEv(Event):
+class ServExpEv:
+    doc = open('out.txt', 'w')
     packetBeingServed = None
-    q = []
-    doc = ''
+    time = 0
+    q = ()
+
+    def __lt__(self, obj2):
+        return self.time <= obj2.time
 
     def execute(self, sim):
         soj_t = self.time - self.packetBeingServed.created
         print('%f\n' % soj_t)
         print('%f' % soj_t, file=self.doc)
-
         self.packetBeingServed = None
 
         if len(self.q.que) != 0:
             packet = self.q.remove()
-            self.insertServ(packet, sim)
+            self.insert_server(packet, sim)
 
-    def insertServ(self, packet, sim):
+    def insert_server(self, packet, sim):
         self.packetBeingServed = packet
-        serviceTime = random.expovariate(1)
-        self.time = sim.now() + serviceTime
-        sim.insertEv(self)
-
-
-class Simulator:
-    time = 0
-    sim_limit = ''
-    event_list = []
-
-    def now(self):
-        return self.time
-
-    def insertEv(self,ev):
-        self.event_list.ins(ev)
-
-    def doAllEvents(self):
-        ev = self.event_list.removefirst()
-        while ev is not None:
-            self.time = ev.time
-
-            if self.time > self.sim_limit:
-                break
-
-            ev.execute(self)
-            ev = self.event_list.removefirst()
+        service_time = random.expovariate(1)
+        self.time = sim.now() + service_time
+        sim.insert_ev(self)
--- course4/MM1Sim.py
+++ course4/MM1Sim.py
@@ -5,7 +5,6 @@ random.seed(1)
 
 doc = open('out.txt', 'w')
 
-
 sim = Simulator()
 sim.event_list = EventList()
 sim.sim_limit = 10000
@@ -17,10 +16,9 @@ s = ServExpEv()
 g.q = q
 q.s = s
 s.q = q
-s.doc = doc
 
 g.time = 2
-sim.insertEv(g)
+sim.insert_ev(g)
 
-sim.doAllEvents()
+sim.do_all_events()
 doc.close()
